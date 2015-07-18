9bf6427376de38ef9a4e1fd6ff5917579b618b14
deepworks <deep.choudhuri@gmail.com>
2015-07-18 12:54:31 -0500

    Included more relevant documentation

--- README.md
+++ README.md
@@ -1,30 +1,16 @@
 Motivation :
+------------
 Density functional theory (DFT) based calculations often requires one (e.g. a dilligent grad student / postdoc) to 
 perform convergence calculations on a supercell, prior to examining a research problem . Such calculations typically 
 involve identifying several parameters  like the cut-off energy (to incorporate all the necessary plane wave basis sets), 
 k-spacing (required for integration over the Brilloiun zone), smearing width, etc. These calculations, while important, 
 are extremely tedious, and expensive softwares can happily do them for you.
 
-As a practitioner of  DFT calculations, and a recent python-convert / aficionado / beginner, I took it as an
-opportunity to teach myself python while developing a code to solve a pertinent problem (atleast to me).  
-
-Purpose:
-This python module attempts to automate the process by using already available robust and extensive, open-source python libraries e.g numpy, matplotlib etc. 
-The module provides the user with several tools (in form of classes and methods) to facilitate convergence calculations and analyzes the data.
-
-Code operation:
-1. Automatically creates a directory structure for convergence calculations, containing all the required files
-   Basic files REQUIRED to create the strucutre : POSCAR, POTCAR, KPOINTS, INCAR, VASP.qsub (or any other job submission script), 1-runvasploop.sh and 2-extractData.sh
-    
-2. Use the seprately provided shell script,1-runvasploop.sh, to run the calculations by submitting jobs to your favourite cluster uisng VASP.qsub
-
-3. After the calculations are over,a second shell script, 2-extractData.sh (provided separately), will extract the calculated data, and
-
-4. The plotting utility , using Matplotlib,  will plot energies vs. the relavent parameter. This plot can be further used to identify convergence parameters
+As a practitioner of  DFT calculations, and a recent python-aficionado, I took it as an
+opportunity to solve a pertinent problem (atleast to me).  
 
 Limitations of the Code:
-The code is written using python 2.7, and has been tested on OSX (Yosemite) only.
-The current version only supports VASP (Vienna Ab Inito Simulation Package), however I have tried to keep the classes and methods 
-sufficiently general so that they can be extended to other types of packages (if a dilligent grad student / postdoc wishes to). Presently, the module will assist in the evaluation of ONLY cut-off energy and k-spacing. Since this is a priliminary version, I cannot guarantee that this code could be used as a drop-in substitute and the user will probably have to "tweak" source to suite their needs.
-
-IMPORTANT :  VASP is not included :)
+------------------------
+The code is written using python 2.7 with SPYDER IDE, and is tested on OSX (Yosemite) ONLY. Potential users are reccomended
+to test this module on their respective platforms. The current version is specifically written for VASP. Furthermore, I 
+cannot guarantee that this code could be used "AS IS" and the user may be required "tweak" it to suite their needs.  
--- VASPConvergeUtils.py
+++ VASPConvergeUtils.py
@@ -2,37 +2,41 @@
 """
 Module name : VASPConvergeUtils.py
 
-Motivation :
-Density functional theory (DFT) based calculations often requires one (e.g. a dilligent grad student / postdoc) to 
-perform convergence calculations on a supercell, prior to examining a research problem . Such calculations typically 
-involve identifying several parameters  like the cut-off energy (to incorporate all the necessary plane wave basis sets), 
-k-spacing (required for integration over the Brilloiun zone), smearing width, etc. These calculations, while important, 
-are extremely tedious, and expensive softwares can happily do them for you.
+Author: Deep Choudhuri, PhD
+Date: 18th July 2015
 
-Purpose:
-This python module attempts to automate the aforementioned rocess by using already available robust and extensive, open-source python libraries e.g numpy, matplotlib etc. 
-The module provides the user with several tools (in form of classes and methods) to facilitate convergence calculations and analyzes the data.
+The main idea behind posting this python module is for users to adapt/modify the content to meet their specific goals.
+
 
 Code operation:
-1. Automatically creates a directory structure for convergence calculations, containing all the required files
-   Basic files REQUIRED to create the strucutre : POSCAR, POTCAR, KPOINTS, INCAR, VASP.qsub (or any other job submission script), 1-runvasploop.sh and 2-extractData.sh
+--------------
+1. Automatically creates a directory structure for convergence calculations, 
+   containing all the required files.  Basic files REQUIRED to create the directory 
+   structure are POSCAR, POTCAR, KPOINTS, INCAR, VASP.qsub (or any other job submission script), 
+   1-runvasploop.sh and 2-extractData.sh
     
-2. Use the seprately provided shell script,1-runvasploop.sh, to run the calculations by submitting jobs to your favourite cluster uisng VASP.qsub
+2. Use the seprately provided shell script,1-runvasploop.sh, to run the 
+   calculations by submitting jobs to your favourite cluster using 
+   VASP.qsub (this will require modification depending on the cluster
+   and submission module being used)
 
-3. After the calculations are over,a second shell script, 2-extractData.sh (provided separately), will extract the calculated data, and
+3. After the calculations are over,a second shell script, 2-extractData.sh (provided separately), 
+   will extract the calculated data, and
 
-4. The plotting utility , using Matplotlib,  will plot energies vs. the relavent parameter. This plot can be further used to identify convergence parameters
+4. The plotting utility , using Matplotlib,  will plot energies vs. the relavent parameter. 
+   This plot can be further used for identifying the convergence parameters
+
+IMPORTANT :  The current version DOES NOT connect with VASP (Vienna Ab Inito Simulation Package)
+             on any cluster.
+
 
 Limitations of the Code:
-The code is written using python 2.7, and has been tested on OSX (Yosemite) only.
-The current version only supports VASP (Vienna Ab Inito Simulation Package), however I have tried to keep the classes and methods 
-sufficiently general so that they can be extended to other types of packages (if a dilligent grad student / postdoc wishes to). Presently, the module will assist in the evaluation of ONLY cut-off energy and k-spacing. Since this is a priliminary version, I cannot guarantee that this code could be used as a drop-in substitute and 
-the user probably have to "tweak" it to suite their needs.
+------------------------
+The code is written using python 2.7 with SPYDER IDE, and is tested on OSX (Yosemite) ONLY. Potential users are reccomended
+to test this module on their respective platforms. The current version is specifically written for VASP. Furthermore, I 
+cannot guarantee that this code could be used "AS IS" and the user may be required "tweak" it to suite their needs.  
 
-IMPORTANT :  VASP is not included :)
 
-Author: Deep Choudhuri, PhD
-Date: 7th July 2015
 """
 import os
 import shutil
